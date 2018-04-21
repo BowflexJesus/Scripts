88b5a3b353d5b1a4086253a30e30ed84dcac5e61
BowflexJesus <mattdarby1985@gmail.com>
2018-04-21 00:09:08 -0500

    Initial Commit

---
+++ genRandomBinary.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    char* arg = argv[1];
+    int numVals = atoi(arg);
+    char* fileName;
+    FILE* fp;
+    int i, val;
+
+    /* If argument list is empty, print 20 values by default */
+    if(argc == 2){
+        fileName = argv[1];
+        fp = fopen(fileName, "wb");
+        for(i = 0; i < 20; i++){
+            val = rand()%9999;
+            fwrite(&val, sizeof(int), sizeof(int), fp);
+        }
+    }
+
+    else if(argc == 3){
+        fileName = argv[2];
+        fp = fopen(fileName, "wb");
+        for(i = 0; i < numVals; i++){
+            val = rand()%9999;
+            fwrite(&val, sizeof(int), sizeof(int), fp);
+        }
+    }
+
+    fclose(fp);
+    return 0;
+}
